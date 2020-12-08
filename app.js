4954247af2a8f578739fd4d9aef74b6f5734d386
zigakleine <ziga.kleine@gmail.com>
2020-12-08 16:22:23 +0100

    Heroku push test

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ let express = require('express');
 let path = require('path');
 let routes = require('./routes');
 
-
 let app = express();
-app.set("port", process.env.PORT || 3000 );
+app.set("port", process.env.PORT || 8080 );
 app.use(express.static(path.join(__dirname, '/views')));
 app.use(routes);
 
